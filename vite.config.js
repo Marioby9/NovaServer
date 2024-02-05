f67e88366bb7ce57821b90dfd6e894fbcec066ce
Mario Martín <mmartin.mrmg@gmail.com>
2024-02-05 10:55:42 +0100

    refactor: 🔨 Change Deploy Config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/NovaServer/',
+  base: '/NovaServer/',
   plugins: [
     vue(),
   ],
